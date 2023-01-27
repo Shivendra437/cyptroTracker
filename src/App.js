5551b264e418776c2b5d93dea5f0ea4f16dbb224
Shivender Singh <104500591+Shivendra437@users.noreply.github.com>
2023-01-27 12:23:29 +0530

    working on header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./styles.css";
-
+import "./Style/app.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Component/Header";
+import Homepage from "./Component/Homepage";
+import CoinPage from "./Component/CoinPage";
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/coins/:id" element={<CoinPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
---
+++ src/Component/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AppBar, Container } from "@material-ui/core";
+
+export default function Header() {
+  return (
+    <AppBar>
+      <Container></Container>
+    </AppBar>
+  );
+}
